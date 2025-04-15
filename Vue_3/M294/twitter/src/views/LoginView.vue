3b58f93006556ea9a70592efad0974403469204f
HeiligerG <gggigi111@gmail.com>
2025-04-15 13:25:33 +0200

    F4

--- Vue_3/M294/twitter/src/views/LoginView.vue
+++ Vue_3/M294/twitter/src/views/LoginView.vue
@@ -1,39 +1,58 @@
 <script setup>
 import { ref, computed } from 'vue';
+import { loginUser } from '../api/requests.js'
+import { useRouter } from 'vue-router'
 
 const email = ref('');
 const password = ref('');
+const router = useRouter()
 
 const isLoginButtonDisabled = computed(() => {
   return !email.value || !password.value;
 });
+
+const errors = ref({
+  email: '',
+  password: '',
+})
+
+async function login () {
+  try {
+    await loginUser(email.value, password.value)
+    await router.push('/')
+  } catch (exception) {
+    console.error('login error', exception)
+
+    errors.value = exception.errors
+  }
+}
 </script>
 
 <template>
   <div class="login">
     <section class="login-wrapper">
-      <form action="#" class="login-form" autocomplete="off" novalidate>
+      <form action="#" class="login-form" @submit.prevent="login" autocomplete="off" novalidate>
         <div class="form-group">
           <label class="form-label" for="email">E-Mail</label>
           <input v-model="email" class="form-input" type="email" id="email" />
-          <div class="form-error">
-            Ungültige E-Mail
+          <div class="form-error" v-if="errors.email">
+            {{ errors.email}}
           </div>
         </div>
         <div class="form-group">
           <label class="form-label" for="password">Passwort</label>
           <input v-model="password" class="form-input" type="password" id="password" />
-          <div class="form-error">
-            Falsches Passwort
-          </div>
+          <div class="form-error" v-if="errors.password">
+          {{ errors.password }}
         </div>
-        <div class="form-group">
-          <button :disabled="isLoginButtonDisabled" class="btn btn--primary btn--block">
-            Login
-          </button>
-        </div>
-      </form>
-    </section>
+  </div>
+  <div class="form-group">
+    <button :disabled="isLoginButtonDisabled" class="btn btn--primary btn--block">
+      Login
+    </button>
+  </div>
+  </form>
+  </section>
   </div>
 </template>
 
