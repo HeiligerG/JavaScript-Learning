280de4c766480f910e035680fd7820c34e8f090b
HeiligerG <gggigi111@gmail.com>
2025-04-15 13:46:28 +0200

    F5

--- Vue_3/M294/twitter/src/components/Navigation.vue
+++ Vue_3/M294/twitter/src/components/Navigation.vue
@@ -2,21 +2,30 @@
 import { RouterLink } from 'vue-router';
 import IconHome from './icons/IconHome.vue'
 import IconLogin from './icons/IconLogin.vue'
+import { useAuth } from '../api/auth'
+
+const { isLoggedIn, logout } = useAuth()
+
 </script>
 
 <template>
-    <!-- Navigation -->
-    <nav class="nav">
-        <RouterLink to="/">
-            <IconHome />
-            Home
-        </RouterLink>
-        Stream
-        <RouterLink to="/login">
-            <IconLogin />
-            Login
-        </RouterLink>
-    </nav>
+  <nav class="nav">
+    <RouterLink to="/">
+      <IconHome />
+      Home
+    </RouterLink>
+    <RouterLink to="/stream">
+      Stream
+    </RouterLink>
+    <RouterLink to="/login" v-if="!isLoggedIn">
+      <IconLogin />
+      Login
+    </RouterLink>
+    <a href="#" @click="logout" v-if="isLoggedIn" class="logout-link">
+      <IconLogin />
+      Logout
+    </a>
+  </nav>
 </template>
 
 <style>
