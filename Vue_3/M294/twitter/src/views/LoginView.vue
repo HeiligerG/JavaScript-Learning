eed03c14d750070fb17591bfe5820c6dc9945853
HeiligerG <gggigi111@gmail.com>
2025-04-15 11:15:34 +0200

    D2

--- Vue_3/M294/twitter/src/views/LoginView.vue
+++ Vue_3/M294/twitter/src/views/LoginView.vue
@@ -1,23 +1,34 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+const isLoginButtonDisabled = computed(() => {
+  return !email.value || !password.value;
+});
+</script>
+
 <template>
   <div class="login">
     <section class="login-wrapper">
       <form action="#" class="login-form" autocomplete="off" novalidate>
         <div class="form-group">
           <label class="form-label" for="email">E-Mail</label>
-          <input class="form-input" type="email" id="email" />
+          <input v-model="email" class="form-input" type="email" id="email" />
           <div class="form-error">
             Ungültige E-Mail
           </div>
         </div>
         <div class="form-group">
           <label class="form-label" for="password">Passwort</label>
-          <input class="form-input" type="password" id="password" />
+          <input v-model="password" class="form-input" type="password" id="password" />
           <div class="form-error">
             Falsches Passwort
           </div>
         </div>
         <div class="form-group">
-          <button class="btn btn--primary btn--block">
+          <button :disabled="isLoginButtonDisabled" class="btn btn--primary btn--block">
             Login
           </button>
         </div>
