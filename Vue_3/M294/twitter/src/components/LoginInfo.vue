200e38b3a373861e068daadc385a6e540d77c366
HeiligerG <gggigi111@gmail.com>
2025-04-15 10:31:37 +0200

    C3

--- Vue_3/M294/twitter/src/components/LoginInfo.vue
+++ Vue_3/M294/twitter/src/components/LoginInfo.vue
@@ -1,24 +1,25 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 import IconSparkles from './icons/IconSparkles.vue'
 </script>
 
 <template>
-     <!-- Login Info -->
-     <section class="login-info">
-          <div class="login-info__icon">
-            <IconSparkles/>
-          </div>
-          <div class="login-info__content">
+    <!-- Login Info -->
+    <section class="login-info">
+        <div class="login-info__icon">
+            <IconSparkles />
+        </div>
+        <div class="login-info__content">
             <h2 class="login-info__heading">
-              Du bist nicht angemeldet
+                Du bist nicht angemeldet
             </h2>
             <div class="login-info__action">
-              <a href="#" class="btn btn--transparent">
-                Zum Login &nbsp; &rarr;
-              </a>
+                <RouterLink to="/login" class="btn btn--transparent">
+                    Zum Login &nbsp; &rarr;
+                </RouterLink>
             </div>
-          </div>
-        </section>
+        </div>
+    </section>
 </template>
 
 <style>
