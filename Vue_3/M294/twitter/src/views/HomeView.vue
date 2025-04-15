181432cbe4b7deca7b33c46ba00eacc03d8c1399
HeiligerG <gggigi111@gmail.com>
2025-04-15 11:29:46 +0200

    D3

--- Vue_3/M294/twitter/src/views/HomeView.vue
+++ Vue_3/M294/twitter/src/views/HomeView.vue
@@ -11,7 +11,12 @@ import Tweet from '../components/Tweet.vue'
 
 <!-- Stream -->
 <section class="stream">
-  <Tweet v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"></Tweet>
+  <Tweet 
+    v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+    :user="{ id: 1, name: 'John Doe'}"
+    :text="'Lorem ipsum dolor med'"
+    :created-at="'2023-01-01T12:00:00Z'"
+/>
 </section>
 </template>
 
