a79b548b25f4c0ad6d0b7f94764e67f6ff635dab
HeiligerG <gggigi111@gmail.com>
2025-04-15 12:56:55 +0200

    E3

--- Vue_3/M294/twitter/src/views/HomeView.vue
+++ Vue_3/M294/twitter/src/views/HomeView.vue
@@ -3,18 +3,33 @@ import LoginInfo from '../components/LoginInfo.vue'
 import Composer from '../components/Composer.vue'
 import Tweet from '../components/Tweet.vue'
 import { onMounted, ref } from 'vue'
+import { fetchStream } from '../api/requests'
+
+const loading = ref(true)
+const tweets = ref([])
 
 onMounted(() => {
     console.log('HomeView mounted')
 })
-
-const loading = ref(true)
 
 onMounted(async () => {
     console.log('HomeView mounted')
     setTimeout(() => {
         loading.value = false
     }, 3000)
+})
+
+onMounted(async () => {
+    loading.value = true
+    try {
+        const stream = await fetchStream()
+        
+        tweets.value = stream
+    } catch (error) {
+        console.error(error)
+    } finally {
+        loading.value = false
+    }
 })
 </script>
 
@@ -28,12 +43,14 @@ onMounted(async () => {
     <div v-if="loading" class="loading">
       Lade Tweets...
     </div>
-  <Tweet v-if="!loading" 
-    v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
-    :user="{ id: 1, name: 'John Doe'}"
-    :text="'Lorem ipsum dolor med'"
-    :created-at="'2023-01-01T12:00:00Z'"
-/>
+    <Tweet
+      v-if="!loading"
+      v-for="tweet in tweets"
+      :key="tweet.id"
+      :user="tweet.user"
+      :text="tweet.text"
+      :created-at="tweet.createdAt"
+    />
 </section>
 </template>
 
