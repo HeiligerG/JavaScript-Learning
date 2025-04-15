60d955ab367f64534bad272319942a8fb55e6ebf
HeiligerG <gggigi111@gmail.com>
2025-04-15 10:51:35 +0200

    D1

--- Vue_3/M294/twitter/src/components/Composer.vue
+++ Vue_3/M294/twitter/src/components/Composer.vue
@@ -1,21 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+
+const text = ref('')
 </script>
 
 <template>
-    <!-- Composer -->
-    <form class="composer">
-          <label class="composer__prompt">Was geht?</label>
-          <textarea maxlength="160" class="composer__textarea" placeholder="Verfasse einen Tweet..."/>
-          <div class="composer__actions">
-            <div class="composer__stats stats">
-              <span class="stats__counter">0</span>
-              <span class="stats__max">/ 160</span>
-            </div>
-            <button disabled class="btn btn--primary">
-              Tweet veröffentlichen
-            </button>
-          </div>
-        </form>
+  <!-- Composer -->
+  <form class="composer">
+    <label class="composer__prompt">Was geht?</label>
+    <textarea v-model="text" maxlength="160" class="composer__textarea" placeholder="Verfasse einen Tweet..." />
+    <div class="composer__actions">
+      <div class="composer__stats stats">
+        <span class="stats__counter">{{ text.length }}</span>
+        <span class="stats__max">/ 160</span>
+      </div>
+      <button :disabled="text.length < 5" class="btn btn--primary">
+        Tweet veröffentlichen
+      </button>
+    </div>
+  </form>
 </template>
 
 <style>
