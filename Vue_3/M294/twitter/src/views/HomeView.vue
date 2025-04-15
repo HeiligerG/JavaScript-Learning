2adb2c15681b7144ab21c549c2b5a9816640006b
HeiligerG <gggigi111@gmail.com>
2025-04-15 11:57:40 +0200

    E1

--- Vue_3/M294/twitter/src/views/HomeView.vue
+++ Vue_3/M294/twitter/src/views/HomeView.vue
@@ -2,6 +2,20 @@
 import LoginInfo from '../components/LoginInfo.vue'
 import Composer from '../components/Composer.vue'
 import Tweet from '../components/Tweet.vue'
+import { onMounted, ref } from 'vue'
+
+onMounted(() => {
+    console.log('HomeView mounted')
+})
+
+const loading = ref(true)
+
+onMounted(async () => {
+    console.log('HomeView mounted')
+    setTimeout(() => {
+        loading.value = false
+    }, 3000)
+})
 </script>
 
 <template>
@@ -11,7 +25,10 @@ import Tweet from '../components/Tweet.vue'
 
 <!-- Stream -->
 <section class="stream">
-  <Tweet 
+    <div v-if="loading" class="loading">
+      Lade Tweets...
+    </div>
+  <Tweet v-if="!loading" 
     v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
     :user="{ id: 1, name: 'John Doe'}"
     :text="'Lorem ipsum dolor med'"
